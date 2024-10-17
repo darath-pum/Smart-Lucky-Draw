89709dc3d06df42f80ad925c7cfbb233b9e1b04f
darathpumcoding <darathpum@gmail.com>
2024-10-17 10:33:30 +0700

    push project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Smart Lucky Draw Portal
+Smart Lucky Draw Portal is a web app project where user could create and manage campaigns, prizes and prize pools for lucky draw events.
+This repository contains 2 projects:
+- dashboard: This is the web app project that contains dashboard UI for user to use
+- server: This is the backend of the portal
+
+## Requirements
+- Node.js version 18.0 or later
+
+## Dashboard Development
+- `cd` into dashboard
+- run `npm install` to install dependencies
+- run `npm run dev` to run the dashboard in development
+
+## Server Development
+- `cd` into server
+- run `npm install` to install dependencies
+- run `npm run dev` to run the server in development
---
+++ server/src/routes/campaign_route.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import {
+  getAllCampaigns,
+  createCampaign,
+  updateCampaign,
+  updateArchiveCampaign,
+  getAllCampaignPrize,
+  getDetailsOfCampaign,
+  deleteCampaignPrizeById,
+  deleteCampaignInvitedEmail,
+  getInvitedEmailAndPermission
+} from "../controllers/campaign_handlers";
+import { checkLevelThreeAndTwo, checkUserLogin } from "../controllers/auth_handlers";
+import multer from "multer";
+import { checkCampaignUpdatePermission, checkCampaignViewPermission } from "../middlewares/campaign_middleware";
+
+const fileUploadHandler = multer();
+const campaignRoute = Router();
+
+
+campaignRoute.get("/getAllCampaigns", checkUserLogin, getAllCampaigns);
+campaignRoute.get("/getDetailsOfCampaigns/:id", checkUserLogin, checkCampaignViewPermission, getDetailsOfCampaign);
+campaignRoute.put("/updateCampaign/:id", checkUserLogin, checkCampaignUpdatePermission, fileUploadHandler.single("participants"), updateCampaign);
+
+campaignRoute.put("/updateArchiveCampaign/:id", updateArchiveCampaign);
+campaignRoute.get("/getAllCampaignPrize/:id", getAllCampaignPrize);
+campaignRoute.delete("/deleteCampaignPrize/:campaignId/:prizeId", deleteCampaignPrizeById);
+campaignRoute.delete("/deleteCampaignInvitedEmail/:campaignId/:email", deleteCampaignInvitedEmail)
+
+campaignRoute.post("/getInvitedEmail", getInvitedEmailAndPermission);
+campaignRoute.use(checkLevelThreeAndTwo());
+campaignRoute.post("/createCampaign", checkUserLogin, fileUploadHandler.single("participants"), createCampaign);
+campaignRoute.post("/getInvitedEmail", getInvitedEmailAndPermission);
+export default campaignRoute;
+
+
